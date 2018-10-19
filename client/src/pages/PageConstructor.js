8560eb7c6a6d9d3098f4b100fc98f3924f520a2f
George Rossakis <george@MacBook-Pro-3.local>
2018-10-19 00:04:57 -0400

    construction page work

--- client/src/pages/PageConstructor.js
+++ client/src/pages/PageConstructor.js
@@ -6,40 +6,67 @@ import API from "../utils/API";
 
 class PageConstructor extends Component {
   state = {
-    box1: {
-      status: "empty"
-    }
+    rows: []
   };
 
-  addElement = () => {
+  // componentDidMount = () => {
+  //   this.setState({
+  //     rows: {
+  //       row1: {
+  //         status: "empty"
+  //       }
+  //     }
+  //   });
+  // };
+
+  componentWillMount = () => {
+    let rows = this.state.rows;
+    let newElement = {
+      status: "heading",
+      content: "This is a Heading",
+      color: "",
+      size: "",
+      font: ""
+    };
+    rows.push(newElement);
+    console.log(rows);
     this.setState({
-      box1: {
-        status: "heading",
-        content: "This is a Heading",
-        color: "",
-        size: "",
-        font: ""
-      }
+      // test: "test"
+      rows: rows
     });
   };
 
+  addElement = () => {
+    console.log("hulloSAM!");
+  };
+
   render() {
-    let element1;
-    if (this.state.box1.status === "empty") {
-      element1 = <FullBox addElement={this.addElement} />;
-    } else if (this.state.box1.status === "heading") {
-      element1 = (
-        <Heading
-          headingColor={this.state.box1.color}
-          headingSize={this.state.box1.size}
-          headingFont={this.state.box1.font}
-          headingContent={this.state.box1.status}
-        />
-      );
-    }
+    console.log(this.state.rows);
+    let rows = this.state.rows;
+    // let element1;
+    // if (this.state.rows === []) {
+    //   element1 = <FullBox addElement={this.addElement} />;
+    // } else if (this.rows[0].status === "heading") {
+    //   element1 = (
+    //     <Heading
+    //       headingColor={this.state.box1.color}
+    //       headingSize={this.state.box1.size}
+    //       headingFont={this.state.box1.font}
+    //       headingContent={this.state.box1.status}
+    //     />
+    //   );
+    // }
+
     return (
       <div className="container">
-        <div className="row">{element1}</div>
+        <div className="row">
+          {rows.map(type => {
+            if (type.status === "heading") {
+              return <Heading headingContent={type.content} />;
+            }
+          })}
+        </div>
+        <button onClick={this.addElement}>add element</button>
       </div>
     );
   }
