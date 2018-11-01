95d02bf7ae4731c0bb29cf4c409fdb1c4bcd4dd6
George Rossakis <george@MacBook-Pro-3.local>
2018-11-01 00:52:05 -0400

    new deploy edits

---
+++ client/build/precache-manifest.9892d6587f699704a989bc8e2de3d496.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cdb1b8819cb1ca3be9415253b6c896f",
+    "url": "/static/media/logo-2.4cdb1b88.png"
+  },
+  {
+    "revision": "5a39cf9095dbe0dad0c8c4da343a7db8",
+    "url": "/static/media/background-pattern.5a39cf90.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b5bc5040a86718e1d76",
+    "url": "/static/js/main.9b5bc504.chunk.js"
+  },
+  {
+    "revision": "dc85e33f26f9d5e1146e",
+    "url": "/static/js/1.dc85e33f.chunk.js"
+  },
+  {
+    "revision": "9b5bc5040a86718e1d76",
+    "url": "/static/css/main.2189ca15.chunk.css"
+  },
+  {
+    "revision": "0b4f2ed0603a3d9d17455ad1312d4a70",
+    "url": "/index.html"
+  }
+];
