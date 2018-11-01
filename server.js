b03ad92ce2accb41d26a7a9d7b0e73e69cde689e
George Rossakis <george@MacBook-Pro-3.local>
2018-11-01 09:15:20 -0400

    another attempt

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ passport.use(
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // Define API routes here
